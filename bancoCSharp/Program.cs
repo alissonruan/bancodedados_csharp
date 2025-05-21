34d895ae80485d4d1056ca201348ada3e6a26e73
Alisson Ruan <alisson.gomes12@etec.sp.gov.br>
2025-05-20 22:28:26 -0300

    banco

---
+++ bancoCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+namespace bancoCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlConnection conexao;
+            conexao = new MySqlConnection("server=localhost;uid=root;password=root");
+            try
+            {
+                conexao.Open();
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
